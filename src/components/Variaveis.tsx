e1ab815d4859b757886a3f1f3f6078a18a12f776
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-04-29 00:04:26 -0300

    variaveis em TS

---
+++ src/components/Variaveis.tsx
@@ -0,0 +1,22 @@
+const Variaveis = () => {
+
+    const name:string = "everton";
+    const age: number = 23;
+    const isWorking : boolean = true;
+
+
+  return (
+    <div>
+        <h1>{name}</h1>
+        <h2>{age}</h2>
+        {isWorking && (
+            <>
+                Está trabalhando!
+            </>
+        )}
+    </div>
+
+  );
+};
+
+export default Variaveis;
